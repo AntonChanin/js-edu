6b50e60fb5859f53d9f8239c87430a0aa7cbfde3
antonvh <antonhaninvl@gmail.com>
2019-10-07 14:04:35 +0300

    feat: add calculate by knowsProgramming equal false

--- src/index.js
+++ src/index.js
@@ -9,6 +9,6 @@ module.exports = function getTimeForEducation(
     knowsProgramming = true,
     config = {family: 4}
     ) {
-    return  Math.ceil(800/config[focus]);
+    return (knowsProgramming === true )? Math.ceil(800/config[focus]) : Math.ceil(1300/config[focus]);
   };
   
